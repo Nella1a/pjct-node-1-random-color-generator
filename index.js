bffcb59cf67770942e02aff85d54acf0d4ebfb5a
Nella1a <m.allen@gmx.at>
2022-01-11 23:50:09 +0100

    Modify userInput variable

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 import AsciiTable from 'ascii-table';
 import chalk from 'chalk';
+// import prompt from 'prompt-sync';
 import randomColor from 'randomcolor';
 
 let colorOutput;
-const userInput = process.argv;
 
-if (userInput.length === 2) {
-  colorOutput = randomColor();
-} else if (userInput.length === 3) {
-  const hueInput = userInput[2];
-  colorOutput = randomColor({
-    hue: hueInput,
-  });
-} else if (userInput.length === 4) {
-  const hueInput = userInput[2];
-  const lumInput = userInput[3];
-  console.log(hueInput, lumInput);
-  colorOutput = randomColor({
-    luminosity: lumInput,
-    hue: hueInput,
+// function generates random color based on requests
+function randomColorOutput(hueI = 'random', lum = 'random') {
+  return randomColor({
+    hue: hueI,
+    luminosity: lum,
   });
 }
 
+// requests for color output
+if (process.argv.length === 2) {
+  colorOutput = randomColor();
+} else if (process.argv.length === 3) {
+  colorOutput = randomColorOutput(process.argv[2]);
+} else if (process.argv.length === 4) {
+  colorOutput = randomColorOutput(process.argv[2], process.argv[3]);
+}
+
+// ouput to terminal
 const table = new AsciiTable('');
 table
   .setBorder(' ')
