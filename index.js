e658bb116d2cb743f76ab80e73c6d01fbde69a02
Nella1a <m.allen@gmx.at>
2022-01-11 22:28:51 +0100

    Add code to index.js, instal and use of npm

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('hello');
+import AsciiTable from 'ascii-table';
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+let colorOutput;
+const userInput = process.argv;
+
+if (userInput.length === 2) {
+  colorOutput = randomColor();
+} else if (userInput.length === 3) {
+  const hueInput = userInput[2];
+  colorOutput = randomColor({
+    hue: hueInput,
+  });
+} else if (userInput.length === 4) {
+  const hueInput = userInput[2];
+  const lumInput = userInput[3];
+  console.log(hueInput, lumInput);
+  colorOutput = randomColor({
+    luminosity: lumInput,
+    hue: hueInput,
+  });
+}
+
+const table = new AsciiTable('');
+table
+  .setBorder(' ')
+  .addRow('########################')
+  .addRow('########################')
+  .addRow('########################')
+  .addRow(`#####              #####`)
+  .addRow(`#####    ${colorOutput}   #####`)
+  .addRow(`#####              #####`)
+  .addRow('########################')
+  .addRow('########################')
+  .addRow('########################');
+console.log(chalk.hex(colorOutput)(table.toString()));
